891f5604dbe7d68db272f8d329fd660ce15f5f5b
JEland-Git-it-done <57952879+JEland-Git-it-done@users.noreply.github.com>
2020-06-02 15:34:02 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Chapter1
+{
+    class Program
+    {
+        int total_cycle_count;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int total_cycle_count = 3;
+            Console.WriteLine(total_cycle_count);
+            Console.WriteLine(total_cycle_count + 3);
+            System.Console.WriteLine("The total number of cycles is {0}", total_cycle_count);
+
+        }
+    }
+}
